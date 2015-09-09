226672778437f56b221d2b2b9636f1742f438f4a
cardvark <endaction@gmail.com>
2015-09-09 08:52:15 -0700

    Fixed 'this' context on MineableObject.compVolDict function using 'bind'

--- code/js/MineableObject.js
+++ code/js/MineableObject.js
@@ -4,33 +4,10 @@ SpaceTrade.MineableObject = function(stats) {
 	this.baseMaterial = stats.baseMaterial || "rock";
 	this.compositionDict = stats.compositionDict || {"rock" : 1.0}
 	this.totalVolume = stats.totalVolume || 10000;
-/*	this.getVolDict = function(compDict, totalVol) {
-		var tempDict = Object.keys(compDict).reduce(function(prev, curr){
-			console.log(prev + ", " + curr);
-			console.log(compDict[curr]);
-			prev[curr] = compDict[curr] * totalVol;
-			return prev;
-		}, {});
-		return tempDict;
-	}*/
-
-	this.compVolDict = Object.keys(this.compositionDict).reduce((prev, curr) => {
+	this.compVolDict = Object.keys(this.compositionDict).reduce(function(prev, curr) {
 		console.log(prev + ", " + curr);
 		console.log(this.compositionDict[curr]);
 		prev[curr] = this.compositionDict[curr] * this.totalVolume;
 		return prev;
-	}, {});
+	}.bind(this), {});
 }
-/*
-	ConstObj = function() {
-		this.compositionDict = {"rock" : 0.8, 
-								"iron" : 0.15, 
-								"gold" : 0.05};
-		this.totalVolume = 10000;
-		this.compVolDict = Object.keys(this.compositionDict).reduce(function(prev, curr){
-			prev[curr] = this.compositionDict[curr] * this.totalVol;
-			return prev;
-		}, {});
-	}
-
-tempObj = new ConstObj();*/
